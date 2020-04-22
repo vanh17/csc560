0529abadefab182b3123037a3c3a0d85abe3f74e
vanh17 <vanh17@hanover.edu>
2020-04-21 19:45:00 -0700

    Implemtend constructor

--- proj4/btree/src/btreefilescan.C
+++ proj4/btree/src/btreefilescan.C
@@ -35,84 +35,26 @@ Status BTreeFileScan::get_next(RID &rid, void *keyptr) {
 		PageId pageNo;
 		Page *leaf_read = new Page();
 		RID curr;
-	}
-
-	if (flag_init == false) {
-		// scan first element
-		pageNo = this->begin;
-		leaf_Read = MINIBASE_DB->read_page(pageNo, leaf_read);
-		memcpy(scan_leaf, leaf_read, sizeof(Page));
-		// find the first record  of a leaf page
-		scan_leaf->firstRecord(first);
-		while (first.slotNo < this->R_Start.slotNo)
-		{
-			scan_leaf->nextRecord(first, next);
-			first = next;
-		}
-
-		// get the first record of a leaf page
-		scan_leaf->HFPage::returnRecord(first, recPtr_comp, rec_Len);
-		if (this->keytype != attrString)
-		{
-			Key_Int *a = (Key_Int *)recPtr_comp;
-
-			memcpy(keyptr, &(a->intkey), sizeof(int));
-			rid = a->data.rid;
-		}
-		else if (this->keytype == attrString)
-		{
-			Key_string *a = (Key_string *)recPtr_comp;
-
-			memcpy(keyptr, a->charkey, sizeof(a->charkey));
-
-			rid = a->data.rid;
-		}
-
-		flag_init = true;
-	} else {
-		if (scan_leaf->nextRecord(first, next) == OK) {
-			// reach the end (the high key), stop
-			if (next.pageNo == this->end && next.slotNo > this->R_End.slotNo)
-				return DONE;
-			scan_leaf->HFPage::returnRecord(next, recPtr_comp, rec_Len);
-
-			if (this->keytype != attrString) {
-				Key_Int *a = (Key_Int *)recPtr_comp;
-
-				memcpy(keyptr, &(a->intkey), sizeof(int));
-
-				rid = a->data.rid;
-			}
-			else if (this->keytype == attrString)
-			{
-				Key_string *a = (Key_string *)recPtr_comp;
-
-				memcpy(keyptr, a->charkey, sizeof(a->charkey));
-
-				rid = a->data.rid;
-			}
-			first = next;
-		} else {
-			PageId next_page = scan_leaf->getPrevPage();
-
-			if (next_page < 0)
-				return DONE; // final page , return
-
-			leaf_Read = MINIBASE_DB->read_page(next_page, leaf_read);
-
-			scan_leaf = (BTLeafPage *)leaf_read;
-			if (scan_leaf->empty())
-				return DONE; // empty page . return DONE
+		if (flag_init == false) {
+			// scan first element
+			pageNo = this->begin;
+			leaf_Read = MINIBASE_DB->read_page(pageNo, leaf_read);
+			memcpy(scan_leaf, leaf_read, sizeof(Page));
+			// find the first record  of a leaf page
 			scan_leaf->firstRecord(first);
-			// reach final page and reach high key rid , stop
-			if (next_page == this->end && first.slotNo >= this->R_End.slotNo)
-				return DONE;
+			while (first.slotNo < this->R_Start.slotNo)
+			{
+				scan_leaf->nextRecord(first, next);
+				first = next;
+			}
+
+			// get the first record of a leaf page
 			scan_leaf->HFPage::returnRecord(first, recPtr_comp, rec_Len);
-			if (this->keytype == attrInteger)
+			if (this->keytype != attrString)
 			{
 				Key_Int *a = (Key_Int *)recPtr_comp;
-				memcpy(keyptr, &(a->intkey), sizeof(int));
 
+				memcpy(keyptr, &(a->intkey), sizeof(int));
 				rid = a->data.rid;
 			}
 			else if (this->keytype == attrString)
@@ -123,12 +65,67 @@ Status BTreeFileScan::get_next(RID &rid, void *keyptr) {
 
 				rid = a->data.rid;
 			}
+
+			flag_init = true;
+		} else {
+			if (scan_leaf->nextRecord(first, next) == OK) {
+				// reach the end (the high key), stop
+				if (next.pageNo == this->end && next.slotNo > this->R_End.slotNo)
+					return DONE;
+				scan_leaf->HFPage::returnRecord(next, recPtr_comp, rec_Len);
+
+				if (this->keytype != attrString) {
+					Key_Int *a = (Key_Int *)recPtr_comp;
+
+					memcpy(keyptr, &(a->intkey), sizeof(int));
+
+					rid = a->data.rid;
+				}
+				else if (this->keytype == attrString)
+				{
+					Key_string *a = (Key_string *)recPtr_comp;
+
+					memcpy(keyptr, a->charkey, sizeof(a->charkey));
+
+					rid = a->data.rid;
+				}
+				first = next;
+			} else {
+				PageId next_page = scan_leaf->getPrevPage();
+
+				if (next_page < 0)
+					return DONE; // final page , return
+
+				leaf_Read = MINIBASE_DB->read_page(next_page, leaf_read);
+
+				scan_leaf = (BTLeafPage *)leaf_read;
+				if (scan_leaf->empty())
+					return DONE; // empty page . return DONE
+				scan_leaf->firstRecord(first);
+				// reach final page and reach high key rid , stop
+				if (next_page == this->end && first.slotNo >= this->R_End.slotNo)
+					return DONE;
+				scan_leaf->HFPage::returnRecord(first, recPtr_comp, rec_Len);
+				if (this->keytype == attrInteger)
+				{
+					Key_Int *a = (Key_Int *)recPtr_comp;
+					memcpy(keyptr, &(a->intkey), sizeof(int));
+
+					rid = a->data.rid;
+				}
+				else if (this->keytype == attrString)
+				{
+					Key_string *a = (Key_string *)recPtr_comp;
+
+					memcpy(keyptr, a->charkey, sizeof(a->charkey));
+
+					rid = a->data.rid;
+				}
+			}
 		}
+		delete leaf_read;
+		// put your code here
 	}
-
-	delete leaf_read;
-
-	// put your code here
 	return OK;
 }
 
